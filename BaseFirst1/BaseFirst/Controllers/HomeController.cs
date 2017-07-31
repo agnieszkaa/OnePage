41b42fa8fae4706044399142115e70b2a1e179d6
DESKTOP-HNDI4QC\Agnieszka Rubik <agnieszka.m.rubik@gmail.com>
2017-07-31 09:42:05 +0200

    no message

---
+++ BaseFirst1/BaseFirst/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using BaseFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaseFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            DataBaseFirstEntities db = new DataBaseFirstEntities();
+            var dane = db.Kontakty;
+            return View(dane);
+        }
+    }
+}
