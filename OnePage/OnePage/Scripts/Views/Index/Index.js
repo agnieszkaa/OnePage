15e90509a0dd3918a81871596b9afa7a782eb92d
DESKTOP-HNDI4QC\Agnieszka Rubik <agnieszka.m.rubik@gmail.com>
2017-08-07 16:05:54 +0200

    goes to homeapi

--- OnePage/OnePage/Scripts/Views/Index/Index.js
+++ OnePage/OnePage/Scripts/Views/Index/Index.js
@@ -1,11 +1,37 @@
-﻿require('HomeApiController.cs')
-
-const ShowContacts = window.Console
+﻿
+//const ShowContacts = window.Console
 
 var indexList = function () {
     debugger
     $.get("HomeApiController/ShowContacts", function (data) {
-        
-        $("table").append(data)
+        debugger
+        //$("table").append(data)
     });
 }(); // IIFE imediately invoke function expression
+$.ajax({url:"/HomeApi/ShowContacts"})
+
+//<apex:page controller="HomeApiController.cs">
+//    <script>
+
+      
+//  </script>
+//</apex:page >
+
+////$_SERVER
+//require('HomeApiController.cs')
+
+//const ShowContacts = window.Console
+//$_SERVER['HTTPS'] = false;
+//var indexList = function () {
+//    debugger
+//    $.get("HomeApiController/ShowContacts", function (data) {
+//        debugger
+//        $("table").append(data)
+//    });
+//}(); // IIFE imediately invoke function expression
+
+////$(function () {
+////    $.getJSON("/HomeApiController/ShowContacts", function (data) {
+////        $("table").append(data)
+////    });
+////});
